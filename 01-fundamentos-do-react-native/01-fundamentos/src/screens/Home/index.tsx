8ee5b038da5bc35f7f575242b80cb4b5afeea7a4
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-08-14 15:34:47 -0300

    M01_02 Aula 02 - Organizando o projeto

---
+++ 01-fundamentos-do-react-native/01-fundamentos/src/screens/Home/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+import {styles} from './styles'
+
+
+export default function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Primeiro projeto</Text>
+
+    </View>
+  );
+}
