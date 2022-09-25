1e5da80ad793ab5224d59e16087173bda8b8fc17
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-09-25 11:55:17 -0300

    M02_02 Aula 05 - Fontes Personalizadas

--- curso/Módulo 02/02-css-in-js/App.tsx
+++ curso/Módulo 02/02-css-in-js/App.tsx
@@ -1,11 +1,16 @@
 import theme from '@theme/index';
 import { ThemeProvider } from 'styled-components';
 import { Groups } from './src/screens/Groups';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
+import { ActivityIndicator } from 'react-native';
+
 
 export default function App() {
+  const [fonstsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fonstsLoaded ? <Groups /> : <ActivityIndicator /> }
     </ThemeProvider>
   );
 }
