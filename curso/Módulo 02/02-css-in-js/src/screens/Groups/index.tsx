fc0507b4e5133bb5d1d5ccf8e8a41fbdfbee6c88
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-09-25 13:28:50 -0300

    M02_02 Aula 11 - Listando grupos

--- curso/Módulo 02/02-css-in-js/src/screens/Groups/index.tsx
+++ curso/Módulo 02/02-css-in-js/src/screens/Groups/index.tsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { Container } from './styles';
+import { FlatList } from 'react-native';
 
 export function Groups() {
+  const [groups, setGroups] = useState(['Galera do Rocket']);
+
   return (
     <Container>
       <Header />
 
-      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+      <Highlight 
+        title="Turmas" 
+        subtitle="Jogue com a sua turma"
+      />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({item}) => <GroupCard title={item} />}
+      />
+      
     </Container>
   );
 }
