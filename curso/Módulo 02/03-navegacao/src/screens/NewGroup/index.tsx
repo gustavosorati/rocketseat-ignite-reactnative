724dd173a1853fedaedbac63369f367ec858ea11
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-09-26 18:23:43 -0300

    M02_03 Aula 07 - Passando parâmetros pela rota

--- curso/Módulo 02/03-navegacao/src/screens/NewGroup/index.tsx
+++ curso/Módulo 02/03-navegacao/src/screens/NewGroup/index.tsx
@@ -3,14 +3,17 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { useNavigation } from "@react-navigation/native";
+import { useState } from "react";
 import { Container, Content, Icon } from "./styles";
 
 
 export function NewGroup() {
+  const [group, setGroup] = useState('');
+
   const navigation = useNavigation();
 
   function handleNew(){
-    navigation.navigate('players', { group: 'rocket'})
+    navigation.navigate('players', { group: group})
   }
 
   return (
@@ -27,6 +30,7 @@ export function NewGroup() {
 
         <Input 
           placeholder="Nome da turma"
+          onChangeText={(text) => setGroup(text)}
         />
 
         <Button
--- curso/Módulo 02/03-navegacao/src/screens/Players/index.tsx
+++ curso/Módulo 02/03-navegacao/src/screens/Players/index.tsx
@@ -10,12 +10,18 @@ import { Container, Form, Icon, HeaderList, NumberOfPlayers } from "./styles";
 import { PlayerCard } from '@components/PlayerCard';
 import { ListEmpty } from '@components/ListEmpty';
 import { Button } from '@components/Button';
+import { useRoute } from '@react-navigation/native';
 
+type RouteParams = {
+  group: string;
+}
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState(['Gustavo']);
+  const [players, setPlayers] = useState([]);
 
+  const route = useRoute();
+  const { group } = route.params as RouteParams;
 
   return (
     <Container>
@@ -24,7 +30,7 @@ export function Players() {
         <Icon />
 
         <Highlight
-          title="Nome da turma"
+          title={group}
           subtitle="Adicione a galera e separe os times"
         /> 
 
