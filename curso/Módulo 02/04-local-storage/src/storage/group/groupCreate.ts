2c13ba72da1edda6318b8d79d1627bf786b5c638
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-09-27 13:13:32 -0300

    M02_04 Aula 03 - Obtendo os grupos

--- curso/Módulo 02/04-local-storage/src/storage/group/groupCreate.ts
+++ curso/Módulo 02/04-local-storage/src/storage/group/groupCreate.ts
@@ -1,13 +1,14 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { GROUP_COLLECTION } from '@storage/storageConfig';
+import { groupGetAll } from './groupsGetAll';
 
 export async function groupCreate(newGroup: string) {
   try {
 
-    const groups = await AsyncStorage.getItem(GROUP_COLLECTION);
+    const groups = await groupGetAll();
+    const storage = JSON.stringify([...groups, newGroup]);
 
-
-    await AsyncStorage.setItem(GROUP_COLLECTION, newGroup);
+    await AsyncStorage.setItem(GROUP_COLLECTION, storage);
     
   } catch (error) {
     throw error;
